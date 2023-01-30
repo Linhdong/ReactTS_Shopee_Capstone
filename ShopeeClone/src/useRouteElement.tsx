28a2d20427d009772591b79f57558fec9de71075
Linhdong <kn.durian.sweet@gmail.com>
2023-01-30 16:07:12 +0800

    set up library and react-router-dom for project
    Signed-off-by: Linhdong <kn.durian.sweet@gmail.com>

---
+++ ShopeeClone/src/useRouteElement.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './page/Login'
+import ProductList from './page/ProductList'
+import Register from './page/Register'
+
+export default function useRouteElement() {
+  const routeElement = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElement
+}
