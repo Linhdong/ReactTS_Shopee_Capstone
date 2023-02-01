b6caa73d948b3c99dd2d6df6abc144c61bc69d90
Linhdong <kn.durian.sweet@gmail.com>
2023-02-01 19:27:32 +0800

    validate confirm_password for Register
    Signed-off-by: Linhdong <kn.durian.sweet@gmail.com>

--- ShopeeClone/src/page/Register/Register.tsx
+++ ShopeeClone/src/page/Register/Register.tsx
@@ -1,7 +1,9 @@
+import { watch } from 'fs'
+import { config } from 'process'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
-import { rules } from 'src/utils/rule'
+import { getRules } from 'src/utils/rule'
 
 interface FormData {
   email: string
@@ -13,12 +15,22 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors }
   } = useForm<FormData>()
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
+  const rules = getRules(getValues)
+
+  const onSubmit = handleSubmit(
+    (data) => {
+      console.log(data)
+    },
+    (data) => {
+      const pass = getValues('password')
+      console.log(pass)
+    }
+  )
+
   // console.log('Error: ', errors)
   return (
     <div className='bg-orange'>
@@ -41,6 +53,7 @@ export default function Register() {
                   type='password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Password'
+                  autoComplete='on'
                   {...register('password', rules.password)}
                 />
                 <div className='mt-1 min-h-[1rem] text-sm text-red-600'>{errors.password?.message}</div>
@@ -50,7 +63,10 @@ export default function Register() {
                   type='password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Confirm Password'
-                  {...register('confirm_password', rules.confirm_password)}
+                  autoComplete='on'
+                  {...register('confirm_password', {
+                    ...rules.confirm_password
+                  })}
                 />
                 <div className='mt-1 min-h-[1rem] text-sm text-red-600'>{errors.confirm_password?.message}</div>
               </div>
